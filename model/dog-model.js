f3b7659ae85d7f7c46daacdd524c458237de7035
Asalagi <mypaintedfrog@yahoo.com>
2023-09-01 10:26:45 -0500

    add createDog model

--- model/dog-model.js
+++ model/dog-model.js
@@ -13,6 +13,23 @@ const getDogs = () => {
  });
 };
 
+const createDogs = () => {
+  const { name, breed, color, sex, birthday } = body;
+  console.log('A new dog has been entered into the database:', body);
+  
+  return new Promise((resolve, reject) => {
+    pool.query('INSERT INTO dogs (name, breed, color, sex, birthday) VALUES ($1, $2, $3, $4,$5) RETURNING *', 
+    [name, breed, color, sex, birthday], (error, results) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(results.rows[0]);
+      }
+    }); 
+  });
+};
+
 module.exports = {
     getDogs,
+    createDogs,
 };
