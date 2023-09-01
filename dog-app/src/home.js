2eac1b5eedcf8464da3660e1b5babf972107dd3c
Asalagi <mypaintedfrog@yahoo.com>
2023-09-01 15:37:38 -0500

    add basic css footer needs help

--- dog-app/src/home.js
+++ dog-app/src/home.js
@@ -5,14 +5,25 @@ import './dog-app.css';
 
 function Home() {
     return (
-        <div className="container">
+        <div>
+            <div className="header-content">
+                <h3 className="head-bold">Register in the month of September and receive a special gift!</h3></div>
             <div className="link-content">
-                <img src={dog} alt="dog" /><br/>
+                <img src={dog} alt="dog" />
                 <h2>DogDog Registry</h2><br/>
                     <Link to="/home">Home</Link><br/>
                     <Link to="/register">Register</Link><br/>
                     Registered<br/>
             </div>
+            <div className="main-content">
+              <div className="inner-content"> 
+            <h1 className="center">Welcome!</h1>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum leo vel orci porta non pulvinar neque. Mauris pharetra et ultrices neque ornare aenean euismod elementum nisi. Eget velit aliquet sagittis id. Quam nulla porttitor massa id neque aliquam. Viverra adipiscing at in tellus. Vivamus at augue eget arcu dictum. Suscipit tellus mauris a diam maecenas sed enim. Pulvinar mattis nunc sed blandit libero volutpat sed. Ut faucibus pulvinar elementum integer enim neque. Morbi tempus iaculis urna id volutpat lacus laoreet non curabitur. Suscipit adipiscing bibendum est ultricies integer quis auctor elit sed. Leo a diam sollicitudin tempor id eu nisl nunc. Nulla facilisi etiam dignissim diam. Ac orci phasellus egestas tellus rutrum tellus pellentesque eu.</p>
+             <p>Vitae aliquet nec ullamcorper sit amet risus nullam eget. Aliquet porttitor lacus luctus accumsan. Odio facilisis mauris sit amet massa. Odio tempor orci dapibus ultrices in. Nunc lobortis mattis aliquam faucibus purus in. Sollicitudin tempor id eu nisl nunc mi. Lectus quam id leo in vitae. Enim nec dui nunc mattis. Mauris ultrices eros in cursus turpis. Elementum integer enim neque volutpat ac tincidunt. Metus aliquam eleifend mi in. Nisi quis eleifend quam adipiscing vitae. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. Sit amet mauris commodo quis imperdiet. Molestie a iaculis at erat pellentesque adipiscing commodo elit at. Est velit egestas dui id ornare arcu odio ut sem.</p>
+             <p>Torta non pulvinar neque laoreet suspendisse interdum consectetur libero. Arcu dui vivamus arcu felis bibendum ut. Ut tellus elementum sagittis vitae et leo. Donec et odio pellentesque diam volutpat commodo sed egestas. Ut pharetra sit amet aliquam id. Mauris in aliquam sem fringilla ut morbi tincidunt. Eget magna fermentum iaculis eu non. Nunc scelerisque viverra mauris in. Dui vivamus arcu felis bibendum ut tristique et egestas. Nullam vehicula ipsum a arcu cursus vitae congue. Gravida neque convallis a cras. Consectetur adipiscing elit duis tristique sollicitudin nibh. Faucibus scelerisque eleifend donec pretium vulputate sapien. Vitae purus faucibus ornare suspendisse.</p>
+            </div>
+            </div> 
+            <footer>Here is a happy little footer</footer>
         </div>
     );
 }
--- model/dog-model.js
+++ model/dog-model.js
@@ -16,7 +16,7 @@ const getDogs = () => {
 const createDogs = () => {
   const { name, breed, color, sex, birthday } = body;
   console.log('A new dog has been entered into the database:', body);
-  
+
   return new Promise((resolve, reject) => {
     pool.query('INSERT INTO dogs (name, breed, color, sex, birthday) VALUES ($1, $2, $3, $4,$5) RETURNING *', 
     [name, breed, color, sex, birthday], (error, results) => {
