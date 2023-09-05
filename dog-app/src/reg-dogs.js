560da38dc9d06837a7b059d64a72c72b9d54dc29
Asalagi <mypaintedfrog@yahoo.com>
2023-09-05 11:12:37 -0500

    add registered

--- dog-app/src/reg-dogs.js
+++ dog-app/src/reg-dogs.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import dog from './dogdog.png';
 import { Link } from 'react-router-dom';
 import './dog-app.css';
+import axios from 'axios';
 
 function RegisteredDogs() {
+    const [dogs, setDogs] = useState([]);
+
+    useEffect(() => {
+        fetchDogs();
+    }, []);
+
+    const fetchDogs = () => {
+        axios.get('http://localhost:3001/dogs')
+        .then(response => {
+            if(Array.isArray(response.data)) {
+                setDogs(response.data);
+            } else {
+                console.error('The server response was not an arrau');
+            }
+        })
+        .catch(error => console.error(`You let go of the leash now your dog ran away: ${error}`))
+    };
+
     return (
         <div className="flex-container">
             <div className="header-content">
@@ -17,7 +36,25 @@ function RegisteredDogs() {
             </div>
             <div className="main-content">
               <div className="inner-content"> 
-            <h1 className="center">Registered Dogs!</h1>       
+            <h1 className="center">Registered Dogs!</h1>  
+            <div className="reg-container">
+                <div className="reg-row">
+                    <div className="reg-category"><h3>Name</h3></div>
+                    <div className="reg-category"><h3>Breed</h3></div>
+                    <div className="reg-category"><h3>Color</h3></div>
+                    <div className="reg-category"><h3>Sex</h3></div>
+                    <div className="reg-category"><h3>Controls</h3></div>
+                </div>
+                {dogs.map(dogs => (
+                <div className="reg-row" key={dogs.id}>
+                    <div className="reg-item">{dogs.name}</div>
+                    <div className="reg-item">{dogs.breed}</div>
+                    <div className="reg-item">{dogs.color}</div>
+                    <div className="reg-item">{dogs.sex}</div>
+                    <div className="reg-item">view page</div>
+                </div>
+                ))}
+            </div>     
             </div>
             </div> 
             <footer>Here is a happy little footer</footer>
